3c75c340e039d56cdfd10ff70459236ce5455eb0
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-07 14:52:54 +0530

    Update RestaurantException.java

--- main/Exception/RestaurantException.java
+++ main/Exception/RestaurantException.java
@@ -1,1 +1,19 @@
+package com.food.exception;
+
+public class RestaurantException extends Exception 
+{
+	
+
+	public RestaurantException()
+	{
+		super();
+	}
+
+	public RestaurantException(String message)
+	{
+		super(message);
+	}
+	
+
+}
 
