1d3537edf5f2e13206ccfc96e11ff0ed5ee0dab8
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-07 14:46:11 +0530

    Update CategoryException.java

--- main/Exception/CategoryException.java
+++ main/Exception/CategoryException.java
@@ -1,1 +1,15 @@
+package com.food.exception;
+
+public class CategoryException extends Exception
+{
+	public CategoryException() {
+
+	}
+
+	public CategoryException(String msg) {
+
+		super(msg);
+	}
+
+}
 
