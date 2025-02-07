8f578bd1359373f705a0f6ac7a2f191fbb4ee98a
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-07 14:42:23 +0530

    Update BillException.java

--- main/Exception/BillException.java
+++ main/Exception/BillException.java
@@ -1,1 +1,17 @@
+package com.food.exception;
+
+public class BillException extends Exception
+{
+	public BillException() 
+	{
+
+	}
+
+	public BillException(String message) 
+	{
+
+		super(message);
+	}
+
+}
 
