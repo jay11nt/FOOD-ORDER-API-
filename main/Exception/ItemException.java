190948b817db55c45bbb7b702b9786e260e3b16f
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-07 14:50:27 +0530

    Update ItemException.java

--- main/Exception/ItemException.java
+++ main/Exception/ItemException.java
@@ -1,1 +1,18 @@
+package com.food.exception;
+
+public class ItemException extends Exception 
+{
+
+	
+	public ItemException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ItemException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
 
