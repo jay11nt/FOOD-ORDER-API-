a8fbbb6faf71cab0011a8332a1992a032794835d
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-07 14:51:16 +0530

    Update LoginException.java

--- main/Exception/LoginException.java
+++ main/Exception/LoginException.java
@@ -1,1 +1,15 @@
+package com.food.exception;
+
+public class LoginException extends Exception
+{
+	public LoginException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public LoginException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+
+}
 
