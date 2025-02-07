763bfa29c9fd85217c6c71b2396e479e6809362f
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-07 14:47:13 +0530

    Update CustomerNotFound.java

--- main/Exception/CustomerNotFound.java
+++ main/Exception/CustomerNotFound.java
@@ -1,1 +1,20 @@
+package com.food.exception;
+
+import lombok.Data;
+
+
+public class CustomerNotFound extends Exception
+{
+
+	public CustomerNotFound() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomerNotFound(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
 
