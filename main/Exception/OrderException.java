beb04c78a5a7a4e0caae798be80de0f850d2b9d1
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-07 14:52:02 +0530

    Update OrderException.java

--- main/Exception/OrderException.java
+++ main/Exception/OrderException.java
@@ -1,1 +1,19 @@
+package com.food.exception;
+
+public class OrderException extends Exception 
+{
+
+	public OrderException() 
+	{
+		super();
+	}
+	
+
+    public OrderException(String message) 
+    {
+        super(message);
+    }
+	
+
+}
 
