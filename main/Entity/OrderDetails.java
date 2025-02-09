19824e03660a39a391d662db0abcd680c652099e
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-09 17:17:24 +0530

    Update OrderDetails.java

--- main/Entity/OrderDetails.java
+++ main/Entity/OrderDetails.java
@@ -1,1 +1,35 @@
+package com.food.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class OrderDetails
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer orderId;
+
+    private LocalDate orderDate;
+
+    private String orderStatus;
+    
+    @OneToOne( cascade = CascadeType.ALL)
+    private FoodCart foodCart;
+
+}
 
