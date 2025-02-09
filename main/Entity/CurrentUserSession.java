0bf552b1030f81801b0957504415aec697b31b32
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-09 17:11:57 +0530

    Update CurrentUserSession.java

--- main/Entity/CurrentUserSession.java
+++ main/Entity/CurrentUserSession.java
@@ -1,1 +1,29 @@
+package com.food.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+public class CurrentUserSession 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer currSessionId;
+	private String email;
+	private LocalDateTime loginDateTime;
+	private String privateKey;
+
+}
 
