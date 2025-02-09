cc6c892187aa7a2cf630d9fbb7dc3e771a4e830b
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-09 17:09:50 +0530

    Update Bill.java

--- main/Entity/Bill.java
+++ main/Entity/Bill.java
@@ -1,1 +1,37 @@
+package com.food.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+public class Bill 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer billId;
+	
+	private LocalDate billDate;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private OrderDetails orderDetails;
+	
+	private Integer totalItem;
+	
+	private Double totalCost;
+
+}
 
