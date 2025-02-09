7bfbc92a00387a035b2e0029d43e8808dcab721d
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-09 17:14:49 +0530

    Update Item.java

--- main/Entity/Item.java
+++ main/Entity/Item.java
@@ -1,1 +1,44 @@
+package com.food.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Item 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer itemId;
+    private String itemName;
+    
+    @OneToOne(cascade = CascadeType.ALL)
+    private Category category;
+    
+    private Integer quantity;
+    
+    private double cost;
+    
+   // @JsonIgnore
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "itemList")
+    private List<Restaurant> restaurant = new ArrayList<>();
+
+}
 
