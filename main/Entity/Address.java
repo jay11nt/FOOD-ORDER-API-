132e925315c2dc036c6c6137bda88d53fa39ae7c
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-09 17:09:04 +0530

    Update Address.java

--- main/Entity/Address.java
+++ main/Entity/Address.java
@@ -1,1 +1,36 @@
+package com.food.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Address 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer addressId;
+	private String buildingName;
+	private String area;
+	private String city;
+	private String state;
+	private String country;
+	private Integer pincode;
+	
+	@OneToOne
+	private Customer customer;
+	
+}
 
