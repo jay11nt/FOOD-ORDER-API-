88fd92295bcadedceddf25d42c785f3ba7e9443a
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-09 17:15:40 +0530

    Update Login.java

--- main/Entity/Login.java
+++ main/Entity/Login.java
@@ -1,1 +1,18 @@
+package com.food.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class Login 
+{
+	private String userName;
+	private String password;
+}
 
