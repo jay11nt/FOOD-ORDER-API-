360e593d9ca35d90d6b950bd65419647bf26a39a
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-09 17:14:03 +0530

    Update FoodCart.java

--- main/Entity/FoodCart.java
+++ main/Entity/FoodCart.java
@@ -1,1 +1,40 @@
+package com.food.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class FoodCart 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer cartId;
+	
+	@JsonBackReference
+	@OneToOne(cascade = CascadeType.ALL)
+	private Customer customer;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Item> itemList= new ArrayList<>();
+	
+
+}
 
