03014c84799b056a493b967fd7b5f52d7a0c3353
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-09 17:18:09 +0530

    Update Restaurant.java

--- main/Entity/Restaurant.java
+++ main/Entity/Restaurant.java
@@ -1,1 +1,45 @@
+package com.food.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Restaurant 
+{
+	@Id
+	@GeneratedValue(strategy =  GenerationType.AUTO)
+	private Integer restaurantId;
+	
+	private String restaurantName;
+	
+	private String managerName;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Address address;
+	
+	@ManyToMany(cascade = CascadeType.ALL)
+	private List<Item> itemList=new ArrayList<>();
+	
+	private String contactNumber;
+}
 
