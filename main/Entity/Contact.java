f54a75b43f3a71d9b260880565caa2030ba6d16a
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-09 17:11:09 +0530

    Update Contact.java

--- main/Entity/Contact.java
+++ main/Entity/Contact.java
@@ -1,1 +1,19 @@
+package com.food.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Contact 
+{
+	private String name;
+    private  String url;
+    private String email;
+
+}
 
