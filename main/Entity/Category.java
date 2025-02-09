49d195edc037d12086c424746fca0924da2bf8b5
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-09 17:10:29 +0530

    Update Category.java

--- main/Entity/Category.java
+++ main/Entity/Category.java
@@ -1,1 +1,27 @@
+package com.food.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Category 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer catId;
+    private String categoryName;
+
+}
 
