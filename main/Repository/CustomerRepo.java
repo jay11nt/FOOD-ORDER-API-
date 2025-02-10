ac3cef5b3c20bf41af0c16ae5264875488229a9e
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-10 19:03:25 +0530

    Update CustomerRepo.java

--- main/Repository/CustomerRepo.java
+++ main/Repository/CustomerRepo.java
@@ -1,1 +1,14 @@
+package com.food.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.food.model.Customer;
+
+@Repository
+public interface CustomerRepo extends JpaRepository<Customer , Integer>
+{
+	public Customer findByEmail(String email);
+
+}
 
