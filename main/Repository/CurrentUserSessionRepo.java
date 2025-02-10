579be117dbbd196f8c2e0420e61a51ec64b88d5c
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-10 19:02:40 +0530

    Update CurrentUserSessionRepo.java

--- main/Repository/CurrentUserSessionRepo.java
+++ main/Repository/CurrentUserSessionRepo.java
@@ -1,1 +1,16 @@
+package com.food.repository;
+
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+import com.food.model.CurrentUserSession;
+
+
+
+public interface CurrentUserSessionRepo extends JpaRepositoryImplementation<CurrentUserSession, Integer>
+{
+	public CurrentUserSession findByEmail(String email);
+	
+	public CurrentUserSession findByPrivateKey(String key);
+
+}
 
