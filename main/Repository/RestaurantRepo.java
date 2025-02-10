98f264ab2b23501dc382d92c2cd0c9936b45ab67
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-10 19:05:58 +0530

    Update RestaurantRepo.java

--- main/Repository/RestaurantRepo.java
+++ main/Repository/RestaurantRepo.java
@@ -1,1 +1,17 @@
+package com.food.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.food.model.Restaurant;
+
+public interface RestaurantRepo extends JpaRepository<Restaurant, Integer>
+{
+		@Query("select s from Restaurant s, Address a where a.city=?1")
+		public List<Restaurant>getNearByResturant(String Location);
+}
+
+
 
