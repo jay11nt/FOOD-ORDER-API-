0a9149efd5326d7c4bf0bac9ddea258a0ae5354d
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-10 19:04:55 +0530

    Update OrderRepo.java

--- main/Repository/OrderRepo.java
+++ main/Repository/OrderRepo.java
@@ -1,1 +1,12 @@
+package com.food.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.food.model.OrderDetails;
+
+@Repository
+public interface OrderRepo extends JpaRepository<OrderDetails, Integer>{
+
+}
 
