f73730b0071b50c7b9b3a5aa20cc09a5431aad20
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-10 19:00:29 +0530

    Update CartRepo.java

--- main/Repository/CartRepo.java
+++ main/Repository/CartRepo.java
@@ -1,1 +1,12 @@
+package com.food.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.food.model.FoodCart;
+
+@Repository
+public interface CartRepo extends JpaRepository<FoodCart, Integer>{
+
+}
 
