974fbbf61974eb05106026075c26f57ae5c67c59
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-10 19:04:04 +0530

    Update ItemRepo.java

--- main/Repository/ItemRepo.java
+++ main/Repository/ItemRepo.java
@@ -1,1 +1,17 @@
+package com.food.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.food.model.Category;
+import com.food.model.Item;
+
+public interface ItemRepo extends JpaRepository<Item, Integer>
+{
+	public List<Item>findByCategory(Category category);
+	
+	public Item findByItemName(String name);
+
+}
 
