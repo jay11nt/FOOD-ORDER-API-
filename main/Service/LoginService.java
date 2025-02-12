4928d90c76695711303fe3022bf72a9aad307fc5
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-12 21:54:50 +0530

    Update LoginService.java

--- main/Service/LoginService.java
+++ main/Service/LoginService.java
@@ -1,1 +1,13 @@
+package com.food.service;
+
+import javax.security.auth.login.LoginException;
+
+import com.food.model.Login;
+
+
+public interface LoginService {
+	public String loginAccount (Login dto)throws LoginException;
+	public String logoutAccount (String key)throws LoginException;
+
+}
 
