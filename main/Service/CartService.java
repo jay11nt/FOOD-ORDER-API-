0630780df390865a5d0518488af3b2a35d68c0cd
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-12 21:52:01 +0530

    Update CartService.java

--- main/Service/CartService.java
+++ main/Service/CartService.java
@@ -1,1 +1,23 @@
+package com.food.service;
+
+import com.food.exception.CartException;
+import com.food.exception.ItemException;
+import com.food.model.FoodCart;
+import com.food.model.Item;
+
+public interface CartService 
+{
+	public FoodCart addFoodCart(FoodCart foodCart)throws CartException;
+	
+	public FoodCart addItemToFoodCart(Integer id, Item item)throws ItemException, CartException;
+	
+	public FoodCart increaseQuantity(Integer id, Item item, Integer quantity)throws ItemException, CartException;
+	
+	public FoodCart reduceQuantity(Integer id, Item item, Integer quantity)throws ItemException, CartException;
+	
+	public FoodCart removeItemFormCart(Integer id, Item item)throws ItemException, CartException;
+	
+	public FoodCart deleteCart(FoodCart cart) throws CartException;
+
+}
 
