ef286ce1850b0e6eb8b11c83e2cb483a3a44c6c4
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-12 21:51:22 +0530

    Update BillService.java

--- main/Service/BillService.java
+++ main/Service/BillService.java
@@ -1,1 +1,26 @@
+package com.food.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.food.exception.BillException;
+import com.food.model.Bill;
+
+public interface BillService 
+{
+	public Bill addBill(Bill bill)throws BillException;
+	
+	public Bill updateBill(Bill bill)throws BillException;
+	
+	public Bill removeBill(Integer id)throws BillException;
+	
+	public Bill viewBill(Integer id)throws BillException;
+	
+	public List<Bill> viewAllBills(LocalDate startDate, LocalDate endDate) throws BillException;
+	
+	public double CalculateTotalCost(Integer id)throws BillException;
+	
+	
+
+}
 
