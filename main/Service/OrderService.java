7b33eaa8d010acb74ef801224eb03d6bf00c2118
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-12 21:55:32 +0530

    Update OrderService.java

--- main/Service/OrderService.java
+++ main/Service/OrderService.java
@@ -1,1 +1,21 @@
+package com.food.service;
+
+import java.util.List;
+
+import com.food.exception.OrderException;
+import com.food.model.OrderDetails;
+
+public interface OrderService 
+{
+	public OrderDetails addOrder(OrderDetails orderDetails) throws OrderException;
+	
+	public OrderDetails updateOrder(OrderDetails order, Integer id) throws OrderException;
+	
+	public OrderDetails removeOrder(Integer orderId) throws OrderException;
+	
+	public OrderDetails viewOrder(Integer orderId) throws OrderException;
+	
+	public List<OrderDetails> getAllOrderDetails() throws OrderException;
+
+}
 
