1bc8d4cf1d1cb3fd6edb387cc3f2f63ec0be878b
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-12 21:56:11 +0530

    Update RestaurantService.java

--- main/Service/RestaurantService.java
+++ main/Service/RestaurantService.java
@@ -1,1 +1,23 @@
+package com.food.service;
+
+import java.util.List;
+
+import com.food.exception.RestaurantException;
+import com.food.model.Restaurant;
+
+public interface RestaurantService 
+{
+	public Restaurant addRestaurant (Restaurant restaurant)throws RestaurantException;
+	
+	public Restaurant viewRestaurantById (Integer restaurantId)throws RestaurantException;
+	
+	public Restaurant updateRestaurantById (Integer restaurantId)throws RestaurantException;
+	
+	public Restaurant removeRestaurantById (Integer restaurantId)throws RestaurantException;
+	
+	public List<Restaurant> viewRestaurntByCity(String location)throws RestaurantException;
+	
+	public List<Restaurant> viewRestaurantByItemName(String location)throws RestaurantException;
+
+}
 
