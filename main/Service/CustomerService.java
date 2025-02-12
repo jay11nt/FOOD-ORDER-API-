7e9de721cb17f1eaa0d3fda58fe905f4e28b1435
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-12 21:53:30 +0530

    Update CustomerService.java

--- main/Service/CustomerService.java
+++ main/Service/CustomerService.java
@@ -1,1 +1,17 @@
+package com.food.service;
+
+import com.food.exception.CustomerNotFound;
+import com.food.model.Customer;
+
+public interface CustomerService
+{
+	public Customer addCustomer (Customer customer)throws CustomerNotFound;
+	
+	public Customer updateCustomer (Customer customer)throws CustomerNotFound;
+	
+	public Customer removeCustomer (Integer customerId)throws CustomerNotFound;
+	
+	public Customer viewCustomer (Integer customerId)throws CustomerNotFound;
+
+}
 
