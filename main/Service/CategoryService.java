eef693240ca62de5e1aa7ccbbb191274d9e0069a
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-12 21:52:37 +0530

    Update CategoryService.java

--- main/Service/CategoryService.java
+++ main/Service/CategoryService.java
@@ -1,1 +1,20 @@
+package com.food.service;
+
+import java.util.List;
+
+import com.food.exception.CategoryException;
+import com.food.model.Category;
+
+public interface CategoryService 
+{
+    public Category addCategory(Category cat)throws CategoryException;
+	
+	public Category updateCategory(Category cat)throws CategoryException;
+	
+	public Category removeCategory(Integer categoryId)throws CategoryException;
+	
+	public Category viewCategory(Integer categoryId)throws CategoryException;
+	
+	public List<Category> viewAllCategory()throws CategoryException;
+}
 
